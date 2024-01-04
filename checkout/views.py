154baf9f88ff88b53c086b354a209b26cbb4423f
juginowakowska <>
2024-01-04 22:02:05 +0000

    stripe added

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OUy0ZApuZfiR6v74rbqDkch0SwQUIA0AucxS5EfmkNKfphPzckcPN3Oaf7S31gSa9ZUk0oKUigSwlzfMdMJAFch00sv4PdbPr',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
